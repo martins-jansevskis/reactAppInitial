78295ec9d4ad707180d41b03496bf4fb4f21ad55
Martins <martins.jansevskis@gmail.com>
2020-10-03 16:28:34 +0300

    React active initial hello world

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View>
+        <Text>Hello, world!</Text>
+        <Text>Hello, name!</Text>
+        <Text>Hello, name!</Text>
+        <Text>Hello, name!</Text>
+        <Image style={{width: 50, height: 50, backgroundColor:"#000"}} source={{
+            uri: 'https://images.app.goo.gl/zkE27QFuyARtLuaA9',
+          }}/>
+      <Image style={{width: 50, height: 50, backgroundColor:"#009"}} source={{
+              uri: 'https://images.app.goo.gl/zkE27QFuyARtLuaA9',
+            }}/>
+        <Image style={{width: 50, height: 50, backgroundColor:"#070"}} source={{
+                uri: 'https://images.app.goo.gl/zkE27QFuyARtLuaA9',
+              }}/>
+      </View>
   );
 }
 
